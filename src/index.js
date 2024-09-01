d6d1b253a5a8d809f2df425d9c025c0e31f5c0f6
Ajay Rawat <48316457+master-ajay@users.noreply.github.com>
2024-09-01 23:52:07 +0530

    feat: setup utils and

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 // require("dotenv").config();
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
+  path: "./env",
 });
 
 connectDB()
+  .then(() => {
 
+    app.on('error',(error)=>{
+        console.error("Error starting server",error);
+    })
 
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server started on http://localhost:${process.env.PORT || 8000}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error);
+    process.exit(1);
+  });
 
 /*
 (async () => {
